cb6694a77561fe40fef615cefe79969b7fb3139b
Goracio999 <gora999@gmail.com>
2022-05-26 13:21:49 +0300

    Reviews adaptation

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@ if (reviewsSwiper) {
   const swiper = new Swiper('.swiper', {
     // Optional parameters
     //   direction: 'vertical',
+    autoHeight: true,
     loop: true,
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true
     },
   });
 }
